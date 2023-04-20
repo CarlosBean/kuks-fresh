5f0bf021804f9fb2f5d6a1a5292b5a0218221521
carlos benavides <1992cab@gmail.com>
2023-04-19 21:48:54 -0500

    feat: added quote component

---
+++ src/components/Quote.tsx
@@ -0,0 +1,14 @@
+const Quote = () => {
+  return (
+    <section className="grid p-10 sm:h-40 sm:place-content-center">
+      <p className="text-xl text-slate-600 sm:text-3xl">
+        "People who loves to eat are always best people"
+      </p>
+      <p className="mt-2 text-right text-lg font-semibold text-red-500 sm:text-2xl">
+        JULIA CHILD
+      </p>
+    </section>
+  );
+};
+
+export default Quote;
