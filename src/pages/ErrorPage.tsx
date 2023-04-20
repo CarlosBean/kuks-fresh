d088b94e0bcca8d3b8bee7c6f99a07b047cfa1bf
carlos benavides <1992cab@gmail.com>
2023-04-19 21:49:23 -0500

    feat: added error page

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,23 @@
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+
+const ErrorPage = () => {
+  const error = useRouteError();
+
+  return (
+    <div className="grid h-screen place-content-center space-y-3 text-slate-600">
+      <h1 className="text-3xl font-medium">Oops!</h1>
+      <div>Sorry, an unexpected error has occurred.</div>
+
+      {isRouteErrorResponse(error) && (
+        <>
+          <div>
+            Error {error.status} - {error.statusText}
+          </div>
+          {error.data?.message && <p>{error.data.message}</p>}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ErrorPage;
