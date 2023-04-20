5ce1a7922ddb541f2ff3f241a96ad1b1fa51cbab
carlos benavides <1992cab@gmail.com>
2023-04-20 01:20:07 -0500

    feat: added home page UI

---
+++ src/pages/Home.tsx
@@ -0,0 +1,25 @@
+import Banner from '../components/Banner';
+import Quote from '../components/Quote';
+import BannerImg from '../assets/images/home.webp';
+import ChipList from '../components/ChipList';
+import SearchBar from '../components/SearchBar';
+import CardList from '../components/CardList';
+
+const Home = () => {
+  return (
+    <div className="space-y-8">
+      <Quote />
+      <Banner img={BannerImg}></Banner>
+      <div className="flex flex-col items-center justify-between gap-y-4 sm:flex-row">
+        <h2 className="text-2xl font-medium tracking-tight text-red-500">
+          RECETAS
+        </h2>
+        <SearchBar />
+      </div>
+      <ChipList />
+      <CardList />
+    </div>
+  );
+};
+
+export default Home;
