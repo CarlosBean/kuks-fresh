55c121d90acfecffdc27ffa819f5a1976d35e508
carlos benavides <1992cab@gmail.com>
2023-04-20 11:48:20 -0500

    chore: added meal service hooks

---
+++ src/api/Meals.ts
@@ -0,0 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+const baseURL = import.meta.env.VITE_API_URL;
+
+const getOne = (id: number) => async () => {
+  const response = await fetch(`${baseURL}/lookup.php?i=${id}`).then(res =>
+    res.json()
+  );
+  return response.meals[0];
+};
+
+const getAllByFilter = (type: string, text: string) => async () => {
+  const response = await fetch(`${baseURL}/filter.php?${type}=${text}`).then(
+    res => res.json()
+  );
+  return response.meals;
+};
+
+export const useOneMeal = (id: number) =>
+  useQuery(['meals', 'detail', id], getOne(id));
+
+export const useAllMealsByFilter = (type: string, text: string) =>
+  useQuery(['meals', type, text, 'list'], getAllByFilter(type, text));
