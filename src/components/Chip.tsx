9f10f91581e645f61da951702c49382fdc9cfad9
carlos benavides <1992cab@gmail.com>
2023-04-20 11:52:05 -0500

    chore: added active classes on selected chip

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -2,17 +2,30 @@ import { Category } from '../models/Category';
 
 type Props = {
   data: Category;
+  onClick: any;
+  isActive: boolean;
 };
 
-const Chip = ({ data }: Props) => {
+const Chip = ({ data, onClick, isActive }: Props) => {
   return (
-    <div className="group flex cursor-pointer items-center gap-2 rounded-3xl bg-slate-100 py-1 pl-2 pr-4 transition duration-200 hover:bg-red-400">
+    <div
+      onClick={onClick}
+      className={`
+        ${isActive ? 'bg-red-400' : 'bg-slate-100'}
+        group flex cursor-pointer items-center gap-2 rounded-3xl py-1 pl-2 pr-4 transition duration-200 hover:bg-red-400
+      `}
+    >
       <img
         src={data.strCategoryThumb}
         alt=""
         className="h-8 rounded-full object-cover"
       />
-      <p className="font-medium text-slate-700 transition duration-200 group-hover:text-white">
+      <p
+        className={`
+          ${isActive ? 'text-white' : 'text-slate-600'}
+          font-medium transition duration-200 group-hover:text-white
+        `}
+      >
         {data.strCategory}
       </p>
     </div>
